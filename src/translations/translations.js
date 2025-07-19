2851fe404e49c35f70efacefa2b292007ea06b12
RicardoPinheiroDev <ricardopintopinheiro2003@gmail.com>
2025-07-19 17:16:39 +0100

    Initial commit

---
+++ src/translations/translations.js
@@ -0,0 +1,115 @@
+export const translations = {
+  pt: {
+    nav: {
+      home: 'Início',
+      skills: 'Habilidades',
+      projects: 'Projetos',
+      language: 'Idioma',
+      selectLanguage: 'Selecionar Idioma',
+      portuguese: 'Português',
+      english: 'Inglês'
+    },
+    home: {
+      greeting: 'Olá,',
+      intro: 'Chamo-me Ricardo Pinheiro, sou um programador web junior que gosta de usar a criatividade para desenvolver soluções inovadoras em aplicações full stack.',
+      downloadCV: 'Baixar CV',
+      copyright: '© 2024 Ricardo Pinheiro'
+    },
+    skills: {
+      title: 'Habilidades',
+      subtitle: 'Tecnologias e ferramentas que domino',
+      chatTitle: 'Escolhe um dos botões para saberes mais sobre mim',
+      frontendButton: 'Tecnologias Frontend',
+      backendButton: 'Tecnologias Backend',
+      toolsButton: 'Ferramentas',
+      androidButton: 'Desenvolvimento Android',
+      databaseButton: 'Base de Dados',
+      responses: {
+        frontend: 'As maravilhas que o frontend trás, não há nada que nos possa separar. Tenho experiência em usar HTML, CSS, Javascript, Bootstrap e React',
+        backend: 'O backend tem sido uma tarefa difícil de sincronizar com o frontend, mas quero sempre dar o meu melhor! Tenho experiência com PHP, Laravel e Filament o que e perfeito para o desenvolvimento rapido de painéis administrativos. Também tenho conhecimentos em NodeJS.',
+        tools: 'Eu sei usar o Git para fazer uploads de projetos no Github ou Gitlab e também tenho conhecimentos basicos sobre o Postman.',
+        android: 'Durante a minha jornada, aprendi a desenvolver aplicações Android. Principalmente Java no Android Studio e também tenho vindo a explorar React Native e Kotlin',
+        database: 'Tenho experiência a trabalhar com Mongodb e SQlite'
+      }
+    },
+    projects: {
+      title: 'Os meus trabalhos',
+      subtitle: 'Alguns dos meus trabalhos recentes',
+      viewProject: 'Ver Projeto',
+      seeOnGithub: 'Ver no GitHub',
+      data: [
+        {
+          id: 'project01',
+          title: 'Projeto 01',
+          description: 'O melhor lugar para encontrar filmes e séries sci-fi',
+          tech: ['React', 'JavaScript', 'CSS'],
+          link: 'https://github.com/RicardoPinheiroDev/TimeofScifi',
+        },
+        {
+          id: 'projectEstagio',
+          title: 'Projeto Estagio Ctesp',
+          description: 'Plataforma de gestão e domínios com área de cliente',
+          tech: ['PHP', 'Laravel', 'MySQL'],
+          link: 'https://github.com/RicardoPinheiroDev/8220611_CTESP_Estagio_DWDM_2025',
+        }
+      ]
+    }
+  },
+  en: {
+    nav: {
+      home: 'Home',
+      skills: 'Skills',
+      projects: 'Projects',
+      language: 'Language',
+      selectLanguage: 'Select Language',
+      portuguese: 'Portuguese',
+      english: 'English'
+    },
+    home: {
+      greeting: 'Hello,',
+      intro: 'My name is Richard Pinewood, I am a junior web developer who likes to use creativity to develop innovative solutions for full stack applications.',
+      downloadCV: 'Download CV',
+      copyright: '© 2024 Richard Pinewood'
+    },
+   
+    skills: {
+      title: 'Skills',
+      subtitle: 'Technologies and tools I master',
+      chatTitle: 'Pick a button to get to know me better',
+      frontendButton: 'Frontend Technologies',
+      backendButton: 'Backend Technologies',
+      toolsButton: 'Tools',
+      androidButton: 'Android Development',
+      databaseButton: 'Databases',
+      responses: {
+        frontend: 'The wonders of frontend are such that nothing can keep us apart. I have experience using HTML, CSS, JavaScript, Bootstrap, and React.',
+        backend: 'Backend has been challenging to sync with frontend, but I always strive to do my best! I have experience with PHP, Laravel, and Filament wich is perfect for building quick admin panels. I\'ve also been focusing more on NodeJS.',
+        tools: 'I know how to use Git, giving me the advantage of uploading projects to GitHub and GitLab. I enjoy using MongoDB for monitoring and building databases and have been learning Postman.',
+        android: 'Throughout my journey, I also have started to the basics of developing Android applications using Java and working with APIs in Android Studio.',
+        database: 'I have experience working with both SQL and NoSQL databases. I\'m comfortable with MySQL for relational data and MongoDB for document-based storage. I also have basic knowledge of PostgreSQL and have been exploring database design and optimization.'
+      }
+    },
+    projects: {
+      title: 'My work',
+      subtitle: 'Some of my recent work',
+      viewProject: 'View Project',
+      seeOnGithub: 'See on GitHub',
+      data: [
+        {
+          id: 'project01',
+          title: 'Project 01',
+          description: 'The best place to find scifi tv shows and movies',
+          tech: ['React', 'JavaScript', 'CSS'],
+          link: 'https://github.com/RicardoPinheiroDev/TimeofScifi',
+        },
+        {
+          id: 'projectEstagio',
+          title: 'Internship Project',
+          description: 'Management and domains platform with client area',
+          tech: ['PHP', 'Laravel', 'MySQL'],
+          link: 'https://github.com/RicardoPinheiroDev/8220611_CTESP_Estagio_DWDM_2025',
+        }
+      ]
+    }
+  }
+}
